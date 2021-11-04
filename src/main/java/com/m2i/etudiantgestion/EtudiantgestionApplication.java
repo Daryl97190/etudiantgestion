832933bb6fc0d312a9c91004ba58685a03e2fec2
Daryl Livio <daryl.livio@gmail.com>
2021-11-04 19:46:34 +0100

    Création Projet gestion étudiant

---
+++ src/main/java/com/m2i/etudiantgestion/EtudiantgestionApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.etudiantgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudiantgestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudiantgestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2i/etudiantgestion/EtudiantgestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.etudiantgestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiantgestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
